44277f62b65c6efba27edd497e2e1d520a06d581
Timur Nizamov <abc@nizamovtimur.ru>
2023-10-15 17:08:01 +0500

    added SUPERUSER_VK_ID env var

--- chatbot/config.py
+++ chatbot/config.py
@@ -6,3 +6,4 @@ load_dotenv(dotenv_path="../.env")
 class Config:
     SQLALCHEMY_DATABASE_URI = f"postgresql://{environ.get('POSTGRES_USER')}:{environ.get('POSTGRES_PASSWORD')}@{environ.get('POSTGRES_HOST')}/{environ.get('POSTGRES_DB')}"
     ACCESS_GROUP_TOKEN = environ.get('ACCESS_GROUP_TOKEN')
+    SUPERUSER_VK_ID = environ.get('SUPERUSER_VK_ID')
