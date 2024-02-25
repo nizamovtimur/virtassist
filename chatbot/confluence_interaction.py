e7d3ec4ae193e88b996a9ed1089074c1997c3f4c
Деканадзе Даниил Давидович <dekanadze2103@mail.ru>
2024-02-25 23:52:23 +0500

    add confluence tree caching using `TTLCache` from `cachetools` module

--- chatbot/confluence_interaction.py
+++ chatbot/confluence_interaction.py
@@ -1,13 +1,15 @@
 ﻿from atlassian import Confluence 
 import bs4
+from cachetools import cached, TTLCache
 
-
+@cached(cache=TTLCache(maxsize=100, ttl=3600))
 def make_markup_by_confluence(confluence: Confluence, space: str) -> list:
     homepage_id = confluence.get_space(space, expand='homepage')["homepage"]["id"]
     pages = confluence.cql(f"parent={homepage_id} and label=\"справка\"")['results']
     return pages
 
 
+@cached(cache=TTLCache(maxsize=100, ttl=3600))
 def parse_confluence_by_page_id(confluence: Confluence, id: int | str) -> list | str:
     pages = confluence.cql(f"parent={id} and label=\"справка\"")['results']
     if len(pages): 
@@ -26,4 +28,3 @@ def parse_confluence_by_page_id(confluence: Confluence, id: int | str) -> list |
             text += i.get_text() + '\n\n'
         if text != '': return text 
         else: return f'Информация находится по ссылке: {page_link}'
-    
