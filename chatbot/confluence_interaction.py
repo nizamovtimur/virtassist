fb53088198b8e996be5a1885f67c69c98e644944
DekDanD <146200996+DekDanD@users.noreply.github.com>
2024-03-15 14:59:37 +0500

    fix `parse_confluence_by_page_id` again

--- chatbot/confluence_interaction.py
+++ chatbot/confluence_interaction.py
@@ -36,7 +36,8 @@ def parse_confluence_by_page_id(id: int | str) -> list | str:
         for i in soup.find_all():
             if i.find("ac:parameter"):
                 i.decompose()
-        for i in soup.find_all(["p", "li", "ul"]):
+        for i in soup.find_all(["p", "li"]):
+            if i.name == 'li': text += '• '
             text += i.get_text() + "\n\n"
         if len(text) == 0:
             return f"Информация находится по ссылке: {page_link}"
