d62aa9db1e403b3066593ad2a13df89e9696c9e7
Герасимов Константин Сергеевич <work.konstantin.gs@gmail.com>
2024-04-08 21:15:14 +0500

    Init new wsgi

--- admin_panel/run.py
+++
@@ -1,7 +0,0 @@
-from config import app
-import views
-
-
-if __name__ == '__main__':
-    app.run(host=app.config['ADMIN_PANEL_HOST'],
-            port=app.config['ADMIN_PANEL_PORT'], debug=True)
---
+++ admin_panel/wsgi.py
@@ -0,0 +1,6 @@
+from config import app
+import views
+
+
+if __name__ == '__main__':
+    app.run()
