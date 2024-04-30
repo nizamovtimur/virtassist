a693b6e75940a0423323285c601d4f38fe460273
Герасимов Константин Сергеевич <work.konstantin.gs@gmail.com>
2024-04-30 20:38:19 +0500

    Init file for Vlad

---
+++ admin-panel/cluster-anaisys.py
@@ -0,0 +1,8 @@
+from models import QuestionAnswer
+
+class ClusterAnalisys():
+    def __init__(self) -> None:
+        pass
+
+    def get_clusters_keywords(self, questions: list[QuestionAnswer]) -> list: #type: ignore
+        pass
