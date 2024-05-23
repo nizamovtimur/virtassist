a951bd2302ac46297611a41e7bf8f54791f82149
Низамов Тимур Дамирович <abc@nizamovtimur.ru>
2024-05-23 22:36:38 +0500

    fix

--- db/migration/env.py
+++ db/migration/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from db.schema import *
+from schema import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
