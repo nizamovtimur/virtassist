4fa459812281ad37130708689ef349b692462fd4
Владислав Евгеньевич Сидоров <vlad.s200@mail.ru>
2024-05-26 10:58:04 +0500

    Add the function of selecting the optimal threshold value for clustering. Increase of the silhouette coefficient from 0.59 to 0.666 Add mark_of_question in models.py and cluster_analysis.py The date of the last question has been replaced with a date interval

--- admin-panel/config.py
+++ admin-panel/config.py
@@ -12,9 +12,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["CHATBOT_HOST"] = os.getenv("CHATBOT_HOST")
 app.config["QA_HOST"] = os.getenv("QA_HOST")
-# app.config["ABBREVIATION_UTMN"] = os.getenv("ABBREVIATION_UTMN").split(" ")
-app.config["ABBREVIATION_UTMN"] = (
-    "тюмгу шкн игип фэи соцгум ипип биофак инзем инхим фти инбио ифк ед шпи шен уиот".split(
-        " "
-    )
-)
+app.config["ABBREVIATION_UTMN"] = os.getenv("ABBREVIATION_UTMN").split(" ")
