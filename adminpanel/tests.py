9ad8d382312d84ec30ca6edf4e5aa97e38db1cf3
Vladislav-Evg-Sid <159266745+Vladislav-Evg-Sid@users.noreply.github.com>
2024-06-06 22:53:57 +0500

    Tests ca (#44)
    * Remove useless code (filter clusters by len)
    Fix bug with strings
    
    * fix by comment
    
    * Add test for cluster_analysis.py
    
    * Refactor TestClusterAnalysis
    Fix bug with function signature
    
    * fix formatting!!!
    
    * Add doc-strings
    
    * Add doc-strings
    
    * Add comments for cod
    
    * Add test to CI-CD
    
    ---------
    
    Co-authored-by: Низамов Тимур Дамирович <abc@nizamovtimur.ru>
    Co-authored-by: Герасимов Константин <work.konstantin.gs@gmail.com>

---
+++ adminpanel/tests.py
@@ -0,0 +1,52 @@
+from cluster_analysis import ClusterAnalysis, mark_of_question
+
+
+class TestClusterAnalysis:
+    """Класс с функцией тестирования анализа вопросов"""
+
+    def test_preprocessing(self):
+        """Тест анализа вопросов"""
+
+        # Импорт тестовых данных
+        arr = []
+        with open("test_db.csv", "r", encoding="utf-8") as f:
+            s = f.readline()
+            while s:
+                x = int(s.split(" --- ")[2])
+                if x == 0:
+                    x = mark_of_question.have_not_answer
+                elif x == 1:
+                    x = mark_of_question.have_low_score
+                elif x == 2:
+                    x = mark_of_question.have_high_score
+                else:
+                    x = mark_of_question.have_not_score
+                arr.append(
+                    {
+                        "text": s.split(" --- ")[0],
+                        "date": s.split(" --- ")[1],
+                        "type": x,
+                    }
+                )
+                s = f.readline()
+        # Обработка
+        CA = ClusterAnalysis()
+        data = CA.get_clusters_keywords(arr)
+        # Сравнение с эталоном
+        with open("test_true_result.csv", "r", encoding="utf-8") as f:
+            assert data[2] == int(f.readline()[:-1])  # Количество кластеров
+            assert data[1] == int(f.readline()[:-1])  # Количество вопросов
+            assert "" == f.readline()[:-1]
+            for ar in data[0]:  # Проверяем каждый кластер
+                assert len(ar[0]) == int(f.readline()[:-1])  # Объём кластера
+                for a in ar[1]:
+                    assert (
+                        a == f.readline()[:-1]
+                    )  # Все ключевые слова и выражения по кластеру
+                assert (
+                    ar[2][0] == f.readline()[:-1]
+                )  # Дата получения чат-ботом первого вопроса в кластере
+                assert (
+                    ar[2][1] == f.readline()[:-1]
+                )  # Дата получения чат-ботом последнего вопроса в кластере
+                assert "" == f.readline()[:-1]
