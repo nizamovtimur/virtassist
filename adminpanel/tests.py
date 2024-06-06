b664bf5837597eb5f863d1c020fa8f6953d8325a
Владислав Евгеньевич Сидоров <vlad.s200@mail.ru>
2024-06-06 08:08:23 +0500

    Add doc-strings

--- adminpanel/tests.py
+++ adminpanel/tests.py
@@ -3,7 +3,11 @@ from cluster_analysis import ClusterAnalysis, mark_of_question
 
 
 class TestClusterAnalysis:
+    """Класс с функцией тестирования анализа вопросов"""
+
     def test_preprocessing(self):
+        """Тест анализа вопросов"""
+
         arr = []
         with open("test_db.csv", "r", encoding="utf-8") as f:
             s = f.readline()
@@ -28,13 +32,21 @@ class TestClusterAnalysis:
         CA = ClusterAnalysis()
         data = CA.get_clusters_keywords(arr)
         with open("test_true_result.csv", "r", encoding="utf-8") as f:
-            assert data[2] == int(f.readline()[:-1])
-            assert data[1] == int(f.readline()[:-1])
+            assert data[2] == int(f.readline()[:-1])  # Количество кластеров
+            assert data[1] == int(f.readline()[:-1])  # Количество вопросов
             assert "" == f.readline()[:-1]
             for ar in data[0]:
-                assert len(ar[0]) == int(f.readline()[:-1])
+                assert len(ar[0]) == int(
+                    f.readline()[:-1]
+                )  # Количество вопросов в кластере
                 for a in ar[1]:
-                    assert a == f.readline()[:-1]
-                assert ar[2][0] == f.readline()[:-1]
-                assert ar[2][1] == f.readline()[:-1]
+                    assert (
+                        a == f.readline()[:-1]
+                    )  # Все ключевые слова и выражения по кластеру
+                assert (
+                    ar[2][0] == f.readline()[:-1]
+                )  # Дата получения чатботом первого вопроса в кластере
+                assert (
+                    ar[2][1] == f.readline()[:-1]
+                )  # Дата последнего вопроса в кластере
                 assert "" == f.readline()[:-1]
